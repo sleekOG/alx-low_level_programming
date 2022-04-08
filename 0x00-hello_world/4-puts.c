c87dc90f70691d8ff8d2589d4e7d5bbfc558cdc3
sleekOG <techbizna@outlook.com>
2022-04-08 00:06:23 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main entry point
+ * 
+ * Return always 0(success)
+ */
+int main(void)
+   {puts("\Programming is like builidng a multilingual puzzle");
+	   return(0);
+   }
