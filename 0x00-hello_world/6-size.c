955f823b945b788e8b5c8e27bc299f6849ce0b84
sleekOG <techbizna@outlook.com>
2022-04-10 14:00:55 -0700

    write a c program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  *     */
 int main(void)
 {
-	printf("size of char: =%i byte\n",sizeof(char));
+	printf("size of char: =%i byte\n", sizeof(char));
 	printf("Size of an int: =%i byte\n",sizeof(int));
-	printf("Size of a long int: =%i byte\n",sizeof(long int));
-	printf("Size of a long long int: =%i bytes\n",sizeof(long long int));
-	printf("Size of a float: =%i bytes\n",sizeof(float));
+	printf("Size of a long int: =%i byte\n", sizeof(long int));
+	printf("Size of a long long int: =%i bytes\n", sizeof(long long int));
+	printf("Size of a float: =%i bytes\n", sizeof(float));
 	return (0);
 }
