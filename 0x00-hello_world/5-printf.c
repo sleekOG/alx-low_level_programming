97bd65b03bc98a23686b9f2c39b25c651da6eee1
sleekOG <techbizna@outlook.com>
2022-04-11 14:29:33 -0700

    task 5 hello ,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main entry point
+ * main - entry of for program
  *
- * Return Always 0 (success)
+ * Description: print the exat sentence
+ * Return: 0 always sucess
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 }
+return (0);
